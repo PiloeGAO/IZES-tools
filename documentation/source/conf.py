4abec37805448a599d9cff07034533e02e37da99
PiloeGAO <leonumerique@gmail.com>
2022-03-01 12:07:09 +0100

    ADD: Documentation: Improve the UI of the doc

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -50,6 +50,21 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+    'prev_next_buttons_location': 'both',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'orange',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
